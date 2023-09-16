3bbd51f69c8c8abf339d9e51e88dc7e6351b7510
Oussama Ezzaou <oezzaou@c1r7p3.1337.ma>
2023-09-16 21:26:16 +0100

    sidjfijdifidj

--- test/test.c
+++ test/test.c
@@ -1,4 +1,5 @@
 # include <mlx.h>
+# include <stdio.h>
 
 int	main(void)
 {
@@ -19,9 +20,21 @@ int	main(void)
 				mlx_pixel_put(mlx_ptr, win_ptr, i * 20, j, 255);
 		}
 	}
+	float	x0, y0;
+
+	x0 = 0;
+	y0 = 0;
 	i = -1;
 	while (++i < 400)
+	{
+		x0 += 10;
+		y0 += 5;
+		if (x0 < 400)
+			printf("P(%f, %f)\n", x0, y0);
 		mlx_pixel_put(mlx_ptr, win_ptr, i, 2 * i, (255 << 16));
+		if (i == 40 && 2 * i == 20)
+			break ;
+	}
 	mlx_loop(mlx_ptr);
 	return (0);
 }
