49b0c75a589f2022f1612114812da900fb9fc467
Oussama Ezzaou <oezzaou@c1r4p7.1337.ma>
2023-09-16 15:53:05 +0100

    isdjfidfijdf

--- test/test.c
+++ test/test.c
@@ -10,18 +10,18 @@ int	main(void)
 	win_ptr = mlx_new_window(mlx_ptr, 400, 400, "Title");
 
 	i = 0;
-	while (++i < 40)
+	while (++i < 20)
 	{
 		j = -1;
 		while (++j < 400)
-				mlx_pixel_put(mlx_ptr, win_ptr, j, i * 10, 255);
-		j = -1;
-		while (++j < 400)
-				mlx_pixel_put(mlx_ptr, win_ptr, 0, i * 10, 255);
+		{
+				mlx_pixel_put(mlx_ptr, win_ptr, j, i * 20, 255);
+				mlx_pixel_put(mlx_ptr, win_ptr, i * 20, j, 255);
+		}
 	}
 	i = -1;
 	while (++i < 400)
-			mlx_pixel_put(mlx_ptr, win_ptr, i, 2 * i, 255);
+		mlx_pixel_put(mlx_ptr, win_ptr, i, 2 * i, (255 << 16));
 	mlx_loop(mlx_ptr);
 	return (0);
 }
